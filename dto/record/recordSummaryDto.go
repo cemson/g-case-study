49feff273ca5e7944bb5330a8c103407072a1c22
Cem Sönmez <cemsnz@gmail.com>
2023-04-12 14:29:48 +0300

    createdAt fix

--- dto/record/recordSummaryDto.go
+++ dto/record/recordSummaryDto.go
@@ -1,7 +1,9 @@
 package record
 
+import "time"
+
 type RecordSummaryDto struct {
-	Key        string `bson:"key" json:"key"`
-	CreatedAt  string `bson:"createdAt" json:"createdAt"`
-	TotalCount int    `bson:"totalCount" json:"totalCount"`
+	Key        string    `bson:"key" json:"key"`
+	CreatedAt  time.Time `bson:"createdAt" json:"createdAt"`
+	TotalCount int       `bson:"totalCount" json:"totalCount"`
 }
